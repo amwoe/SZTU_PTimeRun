c18fdec849471766f193ed41f3deb5e0bb689385
ccmq <cmq129219@126.com>
2024-10-31 23:40:04 +0800

    ccmq

--- pages/kuaididaiqu/kuaidi.js
+++ pages/kuaididaiqu/kuaidi.js
@@ -5,7 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    years:[],
+    months:['1','2','3','4','5','6','7','8','9','10','11','12'],
+    days:[],
+    selectedYear:'',
+    selectedMonth:'1',
+    selectedDay:'1',
   },
   navigateToContact() {
     wx.navigateTo({
@@ -17,7 +22,46 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    const currentYear=new Date().getFullYear();
+    const years=[];
+    for(let i=currentYear;i<=currentYear+5;i++){
+      years.push(i.toString());
+    }
+    this.setData({
+      years:years,
+      selectedYear:currentYear.toString(),
+    });
+    this.updateDays();
+  },
+  onYearChange(e){
+    const year=this.data.years[e.detial.value];
+    this.setData({
+      selectedYear:year,
+    });
+    this.updateDays();
+  },
+  onMonthChange(e){
+    const month=this.data.months[e.detial.value];
+    this.setData({
+      selectedMonth:month,
+    });
+    this.updateDays();
+  },
+  onDayChange(e){
+    const day=e.data.value+1;
+    this.setData({
+      selectedMonth:day,
+    });
+  },
+  updateDays(){
+    const year=parseInt(this.data.selectedYear);
+    const month=parseInt(this.data.selectedMonth);
+    const daysInMonth=new Date(year,month,0).getDate();
+    const days=Array.from({length:daysInMonth},(_,i)=>(i+1).toString());
+    this.setData({
+      days:days,
+      selectedDay:days[0],
+    });
   },
 
   /**
