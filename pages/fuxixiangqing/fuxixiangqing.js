cb2bd26b81650c60e2bd8fa50dd4143a31f0bd66
ccmq <cmq129219@126.com>
2024-11-09 13:29:41 +0800

    ccmq

--- pages/fuxixiangqing/fuxixiangqing.js
+++ pages/fuxixiangqing/fuxixiangqing.js
@@ -1,25 +1,11 @@
-// pages/fuxizhidao/fuxizhidao.js
+// pages/faxiantuijianxiangqing/faxiantuijianxiangqing.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    items: [
-      { imageSrc: "/pages/images/12/矩形 14.png", text: "48天极限过四六级" },
-      { imageSrc: "/pages/images/12/矩形 15（1）.png", text: "概率论与数理统计" },
-      { imageSrc: "/pages/images/12/矩形 14（1）.png", text: "大学生高数自救指南" },
-      { imageSrc: "/pages/images/12/矩形 15.png", text: "大学语文怎么复习" }
-    ],
-  },
 
-  onImageTap:function(e){
-    //获取图片索引
-    const index=e.currentTarget.dataset.index;
-    const item=this.data.items[index];
-    wx.navigateTo({
-      url: '/pages/fuxixiangqing/fuxixiangqing?item='+encodeURIComponent(JSON.stringify(item)),
-    });
   },
 
   /**
