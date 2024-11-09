9241b7af0bad076dc95a3a36e38cbce363009ae0
ccmq <cmq129219@126.com>
2024-11-09 12:06:35 +0800

    ccmq

--- pages/fuxizhidao/fuxizhidao.js
+++ pages/fuxizhidao/fuxizhidao.js
@@ -5,7 +5,21 @@ Page({
    * 页面的初始数据
    */
   data: {
+    items: [
+      { imageSrc: "/pages/images/12/矩形 14.png", text: "48天极限过四六级" },
+      { imageSrc: "/pages/images/12/矩形 15（1）.png", text: "概率论与数理统计" },
+      { imageSrc: "/pages/images/12/矩形 14（1）.png", text: "大学生高数自救指南" },
+      { imageSrc: "/pages/images/12/矩形 15.png", text: "大学语文怎么复习" }
+    ],
+  },
 
+  onImageTap:function(e){
+    //获取图片索引
+    const index=e.currentTarget.dataset.index;
+    const item=this.data.items[index];
+    wx.navigateTo({
+      url: '/pages/faxiantuijianxiangqing/faxiantuijianxiangqing.js?item='+encodeURIComponent(JSON.stringify(item)),
+    });
   },
 
   /**
