c68899823cc12af80c160ae74b3d4421a778f046
ccmq <cmq129219@126.com>
2024-11-13 13:29:30 +0800

    ccmq

--- pages/denglu/denglu.js
+++ pages/denglu/denglu.js
@@ -1,6 +1,30 @@
 Page({
-  data: {},
+  data: {
+    account:'',
+    password:'',
+    isPasswordVisible:false
+  },
 
+  onAccountInput(e){
+    console.log(e.detail.value)
+    this.setData({
+      account:e.detail.value
+    });
+  },
+  onPasswordInput(e){
+    console.log(e.detail.value)
+    this.setData({
+      password:e.detail.value
+    })
+  },
+
+
+  togglePasswordVisibility(){
+    this.setData({
+      isPasswordVisible:!this.data.isPasswordVisible
+    })
+  },
+  
   onShareAppMessage() {
     return {};
   },
