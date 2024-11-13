5d706b27bf0cf852cac8e01db2929bad6514d90c
ccmq <cmq129219@126.com>
2024-11-13 12:45:14 +0800

    ccmq

--- pages/xianwuchushouxiangqing/xianwuchushouxiangqing.js
+++ pages/xianwuchushouxiangqing/xianwuchushouxiangqing.js
@@ -3,6 +3,20 @@ Page({
     items: [null, null],
   },
 
+
+  onConsultationTap:function(){
+    wx.showToast({
+      title: '未留下联系方式...',
+      icon:'none',
+      duration:2000
+    })
+  },
+  onOrderTap:function(){
+    wx.navigateTo({
+      url: '/pages/zujiexiadan/zujiexiadan',
+    })
+  },
+
   onShareAppMessage() {
     return {};
   },
